25ecacfc7fe3935173b694d8935441375e47c454
Lindsay Ledford <lindsayledford0523@gmail.com>
2020-10-13 10:54:28 -0400

    trying to fix navbar

--- ClientApp/src/components/NavBar.jsx
+++ ClientApp/src/components/NavBar.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
+import Reach from 'react'
 import { Link } from 'react-router-dom'
+
 export function NavBar() {
   return (
     <>
       <div className="topNav">
-        <nav className="navbar navbar-expand-lg">
-          <a className="active" href="#home">
-            Home
-          </a>
+        <nav className="navbar navbar-expand-lg navbar-translucent bg-stick">
+          <link className="navbar-brand" to="#">
+            <span className="logo-1" role="img" aria-label="logo"></span>
+          </link>
+
           <a href="#add_new_vacation">New Vacation</a>
           <a href="#user_account_page">My Account</a>
           <a href="#upcoming_vacation">My Upcoming Vacations</a>
         </nav>
       </div>
-
-      <main>
-        <div className="userInfo">Welcome Back, userNameSpace!</div>
-      </main>
+      <div className="userInfo">Welcome Back, userNameSpace!</div>
     </>
   )
 }
