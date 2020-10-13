59f76caf9e2a9ebeffa042a9cb55abb39c85aa52
Lindsay Ledford <lindsayledford0523@gmail.com>
2020-10-13 13:27:12 -0400

    fix logo and navbar css

--- ClientApp/src/components/NavBar.jsx
+++ ClientApp/src/components/NavBar.jsx
@@ -5,18 +5,20 @@ import logo from '../images/logoWL.PNG'
 export function NavBar() {
   return (
     <>
-      <div className="topNav">
-        <nav className="navbar navbar-expand-lg navbar-translucent bg-stick">
+      <nav className="topNav">
+        <div className="logo">
           <Link className="navbar-brand" to="#">
             <img src={logo} className="logo-1" role="img" aria-label="logo" />
           </Link>
+          <div className="userInfo">Welcome Back, userNameSpace!</div>
+        </div>
 
+        <div className="navItems">
           <a href="#add_new_vacation">New Vacation</a>
           <a href="#user_account_page">My Account</a>
           <a href="#upcoming_vacation">My Upcoming Vacations</a>
-        </nav>
-      </div>
-      <div className="userInfo">Welcome Back, userNameSpace!</div>
+        </div>
+      </nav>
     </>
   )
 }
