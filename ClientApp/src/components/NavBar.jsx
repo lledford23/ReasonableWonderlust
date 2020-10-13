4b9d7a2df9ea2ce75bc41a086dcd60729317c20f
Lindsay Ledford <lindsayledford0523@gmail.com>
2020-10-13 12:42:30 -0400

    add logo to navbar

--- ClientApp/src/components/NavBar.jsx
+++ ClientApp/src/components/NavBar.jsx
@@ -1,14 +1,15 @@
-import Reach from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../images/logoWL.PNG'
 
 export function NavBar() {
   return (
     <>
       <div className="topNav">
         <nav className="navbar navbar-expand-lg navbar-translucent bg-stick">
-          <link className="navbar-brand" to="#">
-            <span className="logo-1" role="img" aria-label="logo"></span>
-          </link>
+          <Link className="navbar-brand" to="#">
+            <img src={logo} className="logo-1" role="img" aria-label="logo" />
+          </Link>
 
           <a href="#add_new_vacation">New Vacation</a>
           <a href="#user_account_page">My Account</a>
